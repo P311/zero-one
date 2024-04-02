eafb6576d4fafb9278bb84edefec3080f473853a
linhao liu <linhao.liu98@gmail.com>
2024-04-02 00:33:28 -0400

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PublicComponent } from './components/public/public.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent, PublicComponent],
   imports: [AppRoutingModule, BrowserModule, BrowserAnimationsModule],
-  providers: [],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
