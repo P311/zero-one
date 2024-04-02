e7cd684ddbc348b28ad1ddd93de251e066c6639a
linhao liu <linhao.liu98@gmail.com>
2024-04-02 12:59:36 -0400

    implement scroll animation on nav bar

--- src/app/components/public/public.component.ts
+++ src/app/components/public/public.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-public',
   templateUrl: './public.component.html',
   styleUrl: './public.component.scss',
 })
-export class PublicComponent {}
+export class PublicComponent {
+  @ViewChild('nav') nav: any;
+
+  prevScrollpos = window.pageYOffset;
+
+  @HostListener('window:scroll', [])
+  onScroll(){
+    const currentScrollPos = window.pageYOffset;
+    let nav = document.querySelector("#nav");
+    if (nav){
+      if (this.prevScrollpos > currentScrollPos){
+        nav.classList.add("scroll-down");
+      } else {
+        nav.classList.remove("scroll-down");
+      }
+    }
+    this.prevScrollpos = currentScrollPos;
+  }
+
+}
