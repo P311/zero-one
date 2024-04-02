5b6044071af0e2164453186a106fc37bf83f3a57
linhao liu <linhao.liu98@gmail.com>
2024-04-02 11:23:51 -0400

    initial design of public navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PublicComponent } from './components/public/public.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
-  declarations: [AppComponent, PublicComponent],
+  declarations: [AppComponent],
   imports: [AppRoutingModule, BrowserModule, BrowserAnimationsModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
