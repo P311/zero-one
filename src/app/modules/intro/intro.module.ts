cb2d72557d2e70b163db38d4a969fa818c8f7a44
linhao liu <linhao.liu98@gmail.com>
2024-04-02 12:32:49 -0400

    second section in intro page

--- src/app/modules/intro/intro.module.ts
+++ src/app/modules/intro/intro.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { IntroComponent } from '../../components/intro/intro.component';
 import { IntroRoutingModule } from './intro-routing.module';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [IntroComponent],
-  imports: [CommonModule, IntroRoutingModule,
-      MatButtonModule],
+  imports: [
+    CommonModule,
+    IntroRoutingModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatIconModule,
+  ],
 })
 export class IntroModule {}
