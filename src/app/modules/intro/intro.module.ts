f92cbd4569c5390f4d0b7cc68b563a2417656cf0
linhao liu <linhao.liu98@gmail.com>
2024-04-02 11:24:31 -0400

    first section of intro page

--- src/app/modules/intro/intro.module.ts
+++ src/app/modules/intro/intro.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IntroComponent } from '../../components/intro/intro.component';
 import { IntroRoutingModule } from './intro-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [IntroComponent],
-  imports: [CommonModule, IntroRoutingModule],
+  imports: [CommonModule, IntroRoutingModule,
+      MatButtonModule],
 })
 export class IntroModule {}
