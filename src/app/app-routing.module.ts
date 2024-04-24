af4fff3bc1380e0dfc120c25cb167aa21f117c5f
linhao liu <linhao.liu98@gmail.com>
2024-04-24 00:37:26 -0400

    refactor to auth service and add loading func on login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
-import { authguard } from './guard/auth.guard';
+import { authguard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
