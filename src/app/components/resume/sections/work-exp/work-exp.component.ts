eee5998692b198fbc88f1b28d2c50024833f348b
linhao liu <linhao.liu98@gmail.com>
2024-04-24 23:40:55 -0400

    workexp component add exp section

--- src/app/components/resume/sections/work-exp/work-exp.component.ts
+++ src/app/components/resume/sections/work-exp/work-exp.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MONTHS } from '../../../../globals';
 
 @Component({
   selector: 'app-work-exp',
   templateUrl: './work-exp.component.html',
   styleUrl: './work-exp.component.scss',
 })
-export class WorkExpComponent {}
+export class WorkExpComponent {
+  public currentWork: boolean = false;
+
+  public addWorkExp: boolean = true;
+
+  readonly months = MONTHS;
+  @Output() indexChange = new EventEmitter<number>();
+
+  back() {
+    this.indexChange.emit(0);
+  }
+  submitForm() {
+    // this.indexChange.emit(2);
+    this.addWorkExp = false;
+  }
+
+  currentWorkChange() {
+    this.currentWork = !this.currentWork;
+  }
+}
