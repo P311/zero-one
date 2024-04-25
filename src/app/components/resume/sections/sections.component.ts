8b3e6afcdc7e48106dbf7667eb9fd43c020f9807
linhao liu <linhao.liu98@gmail.com>
2024-04-24 22:58:23 -0400

    redirect to template if not choose

--- src/app/components/resume/sections/sections.component.ts
+++ src/app/components/resume/sections/sections.component.ts
@@ -1,5 +1,6 @@
 import { StepperSelectionEvent } from '@angular/cdk/stepper';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sections',
@@ -8,6 +9,17 @@ import { Component } from '@angular/core';
 })
 export class SectionsComponent {
   public selectedIndex = 0;
+
+  constructor(private activatedRoute: ActivatedRoute, private router: Router){}
+
+  ngOnInit(){
+    this.activatedRoute.queryParams.subscribe(params => {
+      let template = params['template'];
+      if(!template){
+        this.router.navigate(['/resume/template']);
+      }
+    })
+  }
 
   sectionChange(event: StepperSelectionEvent) {
     this.selectedIndex = event.selectedIndex;
