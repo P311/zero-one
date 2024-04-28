8408656451e74242eb0ce3d5929bdf97472669fb
linhao liu <linhao.liu98@gmail.com>
2024-04-28 10:37:12 -0400

    formgroup in header section

--- src/app/components/resume/sections/header/header.component.ts
+++ src/app/components/resume/sections/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { HeaderApi } from '../../../../../../api/api';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +8,33 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  header?: HeaderApi;
+
+  form = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    country: new FormControl(''),
+    state: new FormControl(''),
+    city: new FormControl(''),
+    zipCode: new FormControl(''),
+  });
+
   @Output() indexChange = new EventEmitter<number>();
 
   submitForm() {
+    const val = this.form.value;
+    this.header = {
+      name: val.name,
+      email: val.email,
+      phoneNumber: val.phoneNumber,
+      address: {
+        city: val.city,
+        state: val.state,
+        country: val.country,
+        zipCode: val.zipCode,
+      },
+    };
     this.indexChange.emit(1);
   }
 }
